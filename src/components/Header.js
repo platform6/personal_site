d0d7e110ad2699936d3ff3df361cb7becfc6f82b
platform6 <garrett.peter.conn@gmail.com>
2024-06-20 23:19:51 -0400

    adding routing, adding elden ring api and card results

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 // src/components/Header.js
 import React from 'react';
 import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <Box bg="teal.500" p={4}>
     <Flex justify="space-between" align="center">
-      <Heading as="h1" size="lg" color="white">
-        My Portfolio
-      </Heading>
+      <Link to="/">
+        <Heading as="h1" size="lg" color="white">
+          Garrett Conn
+        </Heading>
+      </Link>
     </Flex>
   </Box>
 );
---
+++ src/components/ResultCard.js
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  Image,
+  VStack,
+  Box,
+} from '@chakra-ui/react';
+
+const ResultCard = ({ data }) => {
+  return (
+    <Card maxW="sm" my="5px" shadow="lg">
+      <CardBody>
+        <VStack align="center" spacing="3">
+          <Heading>{data.name}</Heading>
+          <Image borderRadius="full" boxSize="150px" src={data.image} />
+          <Box maxW="400px">
+            <Text fontSize="lg" justify="center">
+              {data.description}
+            </Text>
+          </Box>
+        </VStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ResultCard;
