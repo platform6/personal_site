c901166039dcfacaac889deaf5f5446c4a8f2f58
platform6 <garrett.peter.conn@gmail.com>
2025-12-18 11:38:27 -0500

    has a intro and sorted card charts

--- src/components/dashboard/SummaryCard.js
+++ src/components/dashboard/SummaryCard.js
@@ -8,17 +8,23 @@ import {
   StatHelpText,
 } from '@chakra-ui/react';
 
-const SummaryCard = ({ label, value, helpText }) => {
+const SummaryCard = ({ stats }) => {
   return (
-    <Card boxShadow={'2xl'}>
+    <Card boxShadow="dark-lg">
       <CardBody>
-        <Stat>
-          <StatLabel fontSize={{ base: 'sm', md: 'md' }}>{label}</StatLabel>
-          <StatNumber fontSize={{ base: '2xl', md: '3xl' }}>{value}</StatNumber>
-          <StatHelpText fontSize={{ base: 'xs', md: 'sm' }}>
-            {helpText}
-          </StatHelpText>
-        </Stat>
+        {stats.map((stat, index) => (
+          <Stat key={index} mb={index < stats.length - 1 ? 4 : 0}>
+            <StatLabel fontSize={{ base: 'sm', md: 'md' }}>
+              {stat.label}
+            </StatLabel>
+            <StatNumber fontSize={{ base: '2xl', md: '3xl' }}>
+              {stat.value}
+            </StatNumber>
+            <StatHelpText fontSize={{ base: 'xs', md: 'sm' }}>
+              {stat.helpText}
+            </StatHelpText>
+          </Stat>
+        ))}
       </CardBody>
     </Card>
   );
